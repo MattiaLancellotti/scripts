9c427cb719b6fdbb508b62d1272351e10e038e88
pixelunknown <lancellottimattia@gmail.com>
2021-10-06 16:18:26 +0200

    New die_on_error function

--- include/system.sh
+++ include/system.sh
@@ -3,3 +3,8 @@
 
 # This function checks whether a command is installed or not.
 check_command() { command -v "$1" 1> /dev/null; }
+
+# This functions should be used when the script needs to print an error
+# message and die. It accepts as first parameter the exit message and the exit
+# code as the second one.
+die_on_error() { echo "$1" >&2; exit $2; }
