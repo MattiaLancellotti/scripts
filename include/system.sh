ec56653d7a50d89bf64fada7674a20ea58068a5b
pixelunknown <lancellottimattia@gmail.com>
2022-01-05 18:54:08 +0100

    Removing unused files

--- include/system.sh
+++
@@ -1,8 +0,0 @@
-#!/bin/sh
-# This file has usefull functions to interact with the operating system.
-
-# This function checks whether a command is installed or not.
-check_command() { command -v "$1" 1> /dev/null; }
-
-# This function checks requirements
-requires_parameters() { [ ${#*} -gt $1 ] && return 0 || return 1; }
